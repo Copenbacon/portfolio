12f4b5f07e8e0be5304ffa520bb4718df3862858
copenbacon <sclary50@gmail.com>
2016-11-11 09:53:11 -0800

    what

--- scripts/models/app.js
+++ scripts/models/app.js
@@ -108,9 +108,16 @@
 
   $('body').on('click', function(){
     $('.header-menu-ul').css({
-      display: 'none',
-      position: 'fixed',
-      width: '100%'
+      'display': 'none',
+      'position': 'fixed',
+      'width': '100%'
+    });
+  });
+
+  $('.header-menu').on('click', function(){
+    $('.header-menu-ul').css({
+      'display': 'block',
+      'list-style-type': 'none'
     });
   });
 
